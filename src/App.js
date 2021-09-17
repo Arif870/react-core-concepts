435d75c6718e4ffba6ebcf776889764ec1e33a4e
Arif870 <arifuzzaman959@gmail.com>
2021-09-17 23:23:57 +0600

    css and JS use in JSX

--- src/App.js
+++ src/App.js
@@ -1,14 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
+  let number = 1234;
+  let profile = {
+    name: "arif-uz-zaman",
+    age: 24,
+    pro: "Developer",
+  };
+  let heading = {
+    fontSize: "45px",
+    fontWeight: 300,
+    color: "orange",
+  };
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
+        <h1 style={heading}>
+          My name is {profile.name} and i am {profile.pro}
+        </h1>
+        <p style={{ fontWeight: "200" }}>My favourite number is {number}</p>
         <a
           className="App-link"
           href="https://reactjs.org"
